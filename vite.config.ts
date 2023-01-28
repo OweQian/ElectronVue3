f861a94daf41e20d53bf7d6f4b19fe26f2280d0c
qian wang <1510106069@qq.com>
2023-01-28 22:43:16 +0800

    feat: 安装包插件

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { devPlugin, getReplacer } from './plugins/devPlugin';
+import { buildPlugin } from './plugins/buildPlugin';
 import optimizer from 'vite-plugin-optimizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      plugins: [buildPlugin()],
+    },
+  },
   plugins: [optimizer(getReplacer()), devPlugin(), vue()],
 })
